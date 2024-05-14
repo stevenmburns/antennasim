a7c6b1ec0349c368e3a5fe93acaefde81f1ef660
Steven Burns <smburns47@gmail.com>
2024-05-13 19:49:34 -0700

    Initial setup file

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+setup(
+    name='antennasim',
+    version='0.0',
+    author='Steve Burns',
+    author_email='smburns47@gmail.com',
+    url='https://github.com/stevenmburns/antennasim/',
+    license='BSD-3-Clause',
+    packages=['antennasim'],
+    python_requires='>=3.10',
+    install_requires=[
+        'numpy',
+        'scipy',
+    ],
+)
